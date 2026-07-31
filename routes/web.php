93b819f6ef9b3615d154f96f55facdd3a697b32e
Victor Martins <v.martins@vehikl.com>
2026-07-31 19:58:45 -0400

    [feat] Adding import via CSV screen

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
 use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\VehicleImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('vehicles.index');
 });
 
+Route::get('vehicles/import', [VehicleImportController::class, 'create'])->name('vehicles.import');
+Route::post('vehicles/import', [VehicleImportController::class, 'store'])->name('vehicles.import-store');
 
 Route::resource('vehicles', VehicleController::class);
 
 Route::get('vehicles/{vehicle}/changeoil', [VehicleController::class, 'showChangeOil'])->name('vehicles.changeoilForm');
 Route::post('vehicles/{vehicle}/changeoil', [VehicleController::class, 'changeOil'])->name('vehicles.changeoil');
+
+
+
