d34a1d1f8d86da50a8b27bfee01528629cd891f6
Victor Martins <v.martins@vehikl.com>
2026-08-19 09:13:21 -0400

    [Fix] - fixing migrations order and owner-vehicles relationship

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -3,18 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Owner extends Model
 {
     protected $fillable = [
-      "name",
-      "email",
-      "phone",
+        "name",
+        "email",
+        "phone",
     ];
 
 
-    public function vehicle() {
-     return $this->hasOne(Vehicle::class);
+    public function vehicles()
+    {
+        return $this->hasMany(Vehicle::class);
     }
 }
